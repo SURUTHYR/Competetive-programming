d2cb4e810bb6d88daebfb9a30574ad4147798d31
Suruthy.R <suruthyravi@gmail.com>
2021-04-09 14:57:35 +0530

    initial commit

---
+++ BasicPrograms/src/Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Addition{
+    public static void main(String[] args) {
+        int a,b,sum;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=a+b;
+        System.out.println("sum is"+sum);
+    }
+
+
+
+
+}
