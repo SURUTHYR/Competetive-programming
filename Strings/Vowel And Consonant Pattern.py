bcbdb4a9b0833d592fd2a49cd890f61d85718c71
Suruthy.R <suruthyravi@gmail.com>
2021-04-15 18:46:17 +0530

    String Pattern Matching

--- Strings/Vowel And Consonant Pattern.py
+++ Strings/Vowel And Consonant Pattern.py
@@ -4,19 +4,33 @@ Time complexity :
 Space complexity : 
 Question link :
 """
-def pattern_matching(s):
-    mylist = list(s)
-    print(mylist)
-    vowel_list = ['a','e','i','o','u']
-    for i in range(len(mylist)):
-        if (i%2==0 and i not in vowel_list) or (i%2!=0 and i in vowel_list):
-                print("cv")
+def consonant_vowel(word):
 
+    for i in range(0,len(word)):
+        if ((i%2==0 and word[i] not in vowel_list) or (i%2!=0 and word[i] in vowel_list)):
+            pass
         else:
-            if (i%2==0 and i  in vowel_list ) or (i%2!=0 and i not in vowel_list):
-                print("vc")
+            return 0
+    return 1
+
+def vowel_consonant(word):
+    for i in range(0,len(words)):
+            if (i%2==0 and word[i]  in vowel_list ) or (i%2!=0 and word[i]  not in vowel_list):
+                pass
+            else:
+                return 0
+    return 1
 
 
+vowel_list = ['a','e','i','o','u']
+s = input("enter the string")
+words = s.split(",")
 
-s = "Badd"
-pattern_matching(s)
+for word in words:
+    word = word.lower()
+    res1 = consonant_vowel(word)
+    res2 = vowel_consonant(word)
+    if(res1==1 or res2==1):
+        print("valid")
+    else:
+        print("invalid")
